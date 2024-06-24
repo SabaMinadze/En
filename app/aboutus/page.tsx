32cd496e45de505cfa8c84e9df1ef9a9e06fb49c
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-24 23:11:37 +0400

    Some translations.

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -6,14 +6,9 @@ function aboutus() {
     <>
       <div className={styles.box}>
         <h1>Hello we are proffesional dedicated Dentists with over 25 years of expereince</h1>
-          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa blanditiis provident
-            at consequatur. Labore, aspernatur ipsum. Qui dicta quidem quis veritatis aspernatur delectus
-              commodi, aut, neque perspiciatis ea consectetur? Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Recusandae ut, fugiat dolorum, voluptates accusamus quasi dicta laboriosam
-                temporibus a veritatis nesciunt aliquam iure nostrum culpa blanditiis aut voluptatibus? Delectus,
-                praesentium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam error tempora
-                  nisi fuga consequatur rerum veniam officia exercitationem nam cumque! Reprehenderit officiis,
-                  deserunt eius accusantium voluptate autem quaerat quam totam.</h1>
+          <h1>Since 2006, our dental clinic has been offering patients all kinds of dental services
+             for both children and adults. The clinic is equipped with the latest equipment and qualified
+              personnel. Our concern is your healthy smile.</h1>
                   <br/><br/><br/><br/>
                   <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa blanditiis provident
             at consequatur. Labore, aspernatur ipsum. Qui dicta quidem quis veritatis aspernatur delectus
