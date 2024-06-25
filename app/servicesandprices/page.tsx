f479327aec1a8e2c8b5a05f51d4fd48e0afe97ba
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-25 11:49:13 +0400

    Update page.tsx

--- app/servicesandprices/page.tsx
+++ app/servicesandprices/page.tsx
@@ -18,47 +18,9 @@ function page() {
     <div className={styles.box}>
     <Card name="diagnostics" price="მკურნალობის ფასი ინდივიდუალურია"/>
     <Card name="Aesthetic dentistry" price="1450 ₾"/>
-    <Card name="ნაფლეთოვანი ოპერაცია (მინიმალურ-ინვაზიური ტექნიკა)" price="500 ₾"/>
+    <Card name="Therapy for adults and children" price="50 -200 ₾"/>
     </div>
-    <div className={styles.box}>
-    <Card name="ნაფლეთოვანი ოპერაცია (დიდი ნაფლეთის ტექნიკა)" price="850 ₾"/>
-    <Card name="შემაერთებელქსოვილოვანი ნაფლეთის ტრანსპლანტაცია სასიდან" price="450 ₾"/>
-    <Card name="ღრძილის რეცესიის დახურვა" price="975 ₾"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="გვირგვინოვანი ნაწილის დაგრძელება" price="650 ₾"/>
-    <Card name="გინგივექტომია" price="150 ₾"/>
-    <Card name="ლაგამის კორექცია" price="300 ₾"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="ვესტიბულოპლასტიკა" price="500 ₾ -დან"/>
-    <Card name="ალვეოლის პრეზერვაცია" price="650 ₾"/>
-    <Card name="სარძევე კბილის მკურნალობა" price="40 - 220 ₾"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="ფისურების ჰერმეტიზაცია" price="65 ₾"/>
-    <Card name="მუდმივი კბილის ექსტრაქცია" price="45 - 170 ₾"/>
-    <Card name="სიბრძნის კბილის ექსტრაქცია" price="110 - 400 ₾"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="იმპლანტი" price="65 ₾"/>
-    <Card name="სინუსლიფტინგი" price="800 ₾ -დან"/>
-    <Card name="ღიმილის ციფრული დაგეგმარება" price="150 ₾"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="გვირგვინი" price="600 - 650 ₾"/>
-    <Card name="ვინირი" price="700 ₾"/>
-    <Card name="პროტეზი" price="510 - 2800 ₾"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="Invisalign" price="9000 - 14000 ₾"/>
-    <Card name="მეტალის ბრეკეტ-სისტემა" price="1100 ₾ -დან"/>
-    <Card name="კერამიკის ბრეკეტ-სისტემა" price="1500 ₾ -დან"/>
-    </div>
-    <div className={styles.box}>
-    <Card name="ლინგვალური ბრეკეტ-სისტემა" price="4700 ₾"/>
-    <Card name="რენტგენოლოგიური გამოკვლევა" price="20 - 85 ₾"/>
-    </div>
+    
 
     </>
   )
