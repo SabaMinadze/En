325672055830c2ee565bac47c20bdcbe0ab4d96b
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-26 20:37:14 +0400

    Change main logo.

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <>
       <div className={styles.navbar}>
-      <Link href="/"><Image src="/alganident.jpg" height={170} width={150} alt="eror" className={styles.logo}/></Link>
+      <Link href="/"><Image src="/algani.png" height={170} width={150} alt="eror" className={styles.logo}/></Link>
       <Link href="/"><h1 className={styles.txt}>Algani Denti</h1></Link>
       <Link href="/"><h1 className={styles.txt2}>Dental Clinic</h1></Link>
       </div>
